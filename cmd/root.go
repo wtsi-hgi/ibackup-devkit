183577e76db1237436516e748f435d8e2f5622db
Yaroslav <ip13@sanger.ac.uk>
2025-06-16 15:29:17 +0100

    HSI-121: make all sets read-only (#1)

---
+++ cmd/root.go
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Genome Research Ltd.
+ *
+ * Author: Iaroslav Popov <ip13@sanger.ac.uk>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+// package cmd is the cobra file that enables subcommands and handles
+// command-line args.
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/inconshreveable/log15"
+	"github.com/spf13/cobra"
+	"github.com/wtsi-hgi/ibackup/set"
+)
+
+// logger is used for logging events in our commands.
+var logger = log15.New()
+
+// RootCmd represents the base command when called without any subcommands.
+var RootCmd = &cobra.Command{
+	Use:   "ibackup-devkit",
+	Short: "Toolkit to work with ibackup database",
+	Long: `ibackup's separate database-altering utility.
+	
+Currently, it can only make all sets read-only.`,
+
+	PersistentPreRun: func(cmd *cobra.Command, _ []string) {
+		// set up logging to stdout
+		logHandler := log15.StreamHandler(cmd.OutOrStdout(), log15.TerminalFormat())
+		logger.SetHandler(log15.LvlFilterHandler(log15.LvlInfo, logHandler))
+	},
+
+	RunE: func(cmd *cobra.Command, _ []string) error {
+		db, err := cmd.Flags().GetString("database")
+		if err != nil {
+			return err
+		}
+
+		makeReadOnly, err := cmd.Flags().GetBool("lock-all-sets")
+		if err != nil {
+			return err
+		}
+
+		if makeReadOnly {
+			return updateDatabase(db)
+		}
+
+		return nil
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags
+// appropriately. This is called by main.main(). It only needs to happen once to
+// the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// global flags
+	RootCmd.Flags().String("database", "", "path to the ibackup database file")
+	RootCmd.Flags().Bool("lock-all-sets", false, "make all sets in the database read-only")
+
+	err := RootCmd.MarkFlagRequired("database")
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func updateDatabase(dbPath string) error {
+	_, err := os.Stat(dbPath)
+	if errors.Is(err, os.ErrNotExist) {
+		return err
+	}
+
+	db, err := set.New(dbPath, "", false)
+	if err != nil {
+		return err
+	}
+
+	allSets, err := db.GetAll()
+	if err != nil {
+		return err
+	}
+
+	logger.Info(fmt.Sprintf("Updating %d sets to read-only mode...", len(allSets)))
+
+	for _, s := range allSets {
+		logger.Info("Updating set", "name", s.Name, "id", s.ID())
+
+		if s.ReadOnly {
+			logger.Info("Set is already read-only, skipping...")
+			continue
+		}
+
+		s.ReadOnly = true
+		err = db.AddOrUpdate(s)
+		if err != nil {
+			logger.Error("Failed to update set. Skipping...")
+			continue
+		}
+
+		logger.Info("Set updated successfully")
+	}
+
+	return db.Close()
+}
---
+++ main_test.go
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Genome Research Ltd.
+ *
+ * Author: Iaroslav Popov <ip13@sanger.ac.uk>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+package main
+
+import (
+	"bytes"
+	"path/filepath"
+	"strconv"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/wtsi-hgi/ibackup-devkit/cmd"
+	"github.com/wtsi-hgi/ibackup/set"
+)
+
+func TestMain(t *testing.T) {
+	Convey("You cannot run a tool without --database flag", t, func() {
+		cmdErrs := new(bytes.Buffer)
+		cmd.RootCmd.SetErr(cmdErrs)
+		cmd.RootCmd.SetOut(cmdErrs)
+
+		err := cmd.RootCmd.Execute()
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldContainSubstring, "required flag(s) \"database\" not set")
+	})
+
+	Convey("Given a test database file", t, func() {
+		testDBFile := filepath.Join(t.TempDir(), "test.db")
+		db, err := set.New(testDBFile, "", false)
+		So(err, ShouldBeNil)
+		for i := range 5 {
+			setName := "set-" + strconv.Itoa(i)
+			s := &set.Set{
+				Name:     setName,
+				ReadOnly: i%2 == 0,
+			}
+			err = db.AddOrUpdate(s)
+			So(err, ShouldBeNil)
+		}
+
+		err = db.Close()
+		So(err, ShouldBeNil)
+
+		cmdErrs := new(bytes.Buffer)
+		cmd.RootCmd.SetOut(cmdErrs)
+
+		Convey("You cannot run a tool on a non-existing database", func() {
+			cmd.RootCmd.SetArgs([]string{"--database", filepath.Join(t.TempDir(), "invalid"), "--lock-all-sets"})
+			err := cmd.RootCmd.Execute()
+			So(err.Error(), ShouldContainSubstring, "no such file or directory")
+		})
+
+		Convey("You can make all sets read-only", func() {
+			cmd.RootCmd.SetArgs([]string{"--database", testDBFile, "--lock-all-sets"})
+
+			err := cmd.RootCmd.Execute()
+			So(err, ShouldBeNil)
+
+			Convey("And all sets will be read-only", func() {
+				db, err := set.New(testDBFile, "", false)
+				So(err, ShouldBeNil)
+
+				allSets, err := db.GetAll()
+				So(err, ShouldBeNil)
+
+				for _, s := range allSets {
+					So(s.ReadOnly, ShouldBeTrue)
+				}
+
+				err = db.Close()
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+}
